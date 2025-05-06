406e8295e0a7745bcd87ec8c9f89fbaaa3fb9f5e
Terrel Shumway <gh@lernisto.com>
2025-05-06 08:43:54 -0600

    I re-created this repository to follow standard deployment pattern

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, loadEnv } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: "/dofftime/",
+    define: {
+      __APP_ENV__: JSON.stringify(env),
+    },
+    plugins: [solidPlugin()],
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: 'esnext',
+    },
+  }
+});
